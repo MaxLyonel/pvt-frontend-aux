da0e29af9f3c69c3f9a82b876d1a818109e1ce82
gladys850 <gpq850@gmail.com>
2021-10-26 15:01:33 -0400

    subir archivos base

---
+++ pages/index.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+
+    <div>
+        <AlbumCard :album="album" v-for="album in albums" :key="album.id"> </AlbumCard>
+    </div>
+</div>
+</template>
+
+<script>
+import axios from 'axios'
+import env from '../config/env'
+import AlbumCard from '../components/AlbumCard'
+
+export default {
+    name: 'index',
+    components: { AlbumCard },
+    data() {
+        return {
+            albums: [],
+        }
+    },
+    created: async function() {
+        let response = await axios.get(`${env.endpoint}/albums`)
+        this.albums = response.data
+    }
+}
+</script>
+
+<style scoped>
+header { 
+    margin-top: 100px; 
+    margin-bottom: 100px;
+}
+</style>
