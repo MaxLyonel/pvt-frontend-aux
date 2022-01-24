debc456f8f8f1dd50c592bc684403a13ffbaaf1d
gladys850 <gpq850@gmail.com>
2022-01-24 00:23:04 -0400

    Crear interfaz y metodos para nuevo registro de usuario en base de datos mediante LDAP

---
+++ pages/administration/addUser.vue
@@ -0,0 +1,12 @@
+<template>
+  <MainAddUser />
+</template>
+
+<script>
+import MainAddUser from '@/components/user/MainAddUser.vue';
+export default {
+  name: "PageAddUser",
+  components: { MainAddUser },
+  props: {},
+};
+</script>
