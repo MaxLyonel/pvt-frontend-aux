2f93298c97d129099b73d9d0f78bee8cd036237c
gladys850 <gpq850@gmail.com>
2022-04-19 11:05:31 -0400

    Cambio de ruta en directorios de importacion y actualizacion de aportes

---
+++ pages/administration/PageRoles.vue
@@ -0,0 +1,12 @@
+<template>
+  <MainRoles />
+</template>
+
+<script>
+import MainRoles from '@/components/role/MainRoles.vue';
+export default {
+  name: "PageRoles",
+  components: { MainRoles },
+  props: {},
+};
+</script>
